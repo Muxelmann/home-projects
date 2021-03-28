09832793fdd1fa7e578c81361842e669d38261f1
Max Zangs <maximilian.zangs@gmail.com>
2021-03-28 20:57:24 +0200

    Putting EPD to sleep before reading the loop's end

--- esp/esp.ino
+++ esp/esp.ino
@@ -9,34 +9,16 @@ const char *password = "SECRET";
 unsigned char connectionSuccess = 30;
 
 unsigned char seg;
-String dataStr;
+String * dataStr;
 int delayTime;
 
 void setup()
 {
   // Start serial connection at 115200 Baud
   Serial.begin(115200);
-
-  Serial.print("[EPD] Initialization");
-  // Create screen (EPD) instance
-   // Initialize / Startup screen
-   if (EPD::Init() < 0)
-   {
-     Serial.println(" --> FAILED");
-     return;
-   }
-   Serial.println(" --> DONE");
-   
-//   // Clear screen
-//   Serial.print("[EPD] Clearing");
-//   EPD::FrameDataStart();
-//   EPD::FrameDataClear();
-//   EPD::FrameDataStop();
-//   EPD::FrameDataShow();
-//   Serial.println(" --> DONE");
-
+  
   // Connect to Wifi
-  Serial.print("[WiFi] Connecting: ");
+  Serial.print("[WiFi]   Connecting: ");
   while (WiFi.status() != WL_CONNECTED)
   {
     connectionSuccess--;
@@ -70,20 +52,32 @@ void loop()
   // Update the MVG data on the server
   DATA::updateImageData();
 
+  Serial.print("[EPD]    Initialization");
+  // Create screen (EPD) instance
+  // Initialize / Startup screen
+  if (EPD::Init() < 0) {
+     Serial.println(" --> FAILED");
+     return;
+  }
+  Serial.println(" --> DONE");
+
   // Load the image and pass it to screen
   EPD::FrameDataStart();
   seg = 0;
   while (seg < SEG_COUNT) {
     dataStr = DATA::getImageData(seg);
-    if (dataStr.length() > 0) {
-      EPD::FrameDataStrBlock(dataStr);
+    if ((*dataStr).length() > 0) {
+      Serial.println("[STATUS] did update segment " + String(seg) + " out of " + String(SEG_COUNT) + " segments");
+      EPD::FrameDataStrBlock(*dataStr);
       seg++;
     } else {
-      Serial.println("[STATUS] did not update segment " + String(SEG_COUNT) + " out of " + String(seg) + " segments");
+      Serial.println("[STATUS] did NOT update segment " + String(seg) + " out of " + String(SEG_COUNT) + " segments");
+      delay(10000);
     }
   }
   EPD::FrameDataStop();
   EPD::FrameDataShow();
+  EPD::Sleep();
 
   delayTime = DATA::getDelayTime();
   Serial.print("[STATUS] waiting " + String(delayTime) + " ms");
